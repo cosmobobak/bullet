81b104174a360f7901400fa87e2c134d74e14231
cosmobobak <cosmobobak@gmail.com>
2025-01-24 18:43:28 +0000

    everything back in

--- examples/viridithas.rs
+++ examples/viridithas.rs
@@ -1,5 +1,5 @@
 use bullet_lib::{
-    default::{inputs::ChessBucketsMirroredFactorised, outputs::MaterialCount},
+    default::{inputs::ChessBucketsMirroredFactorised, outputs::MaterialCountNoisy},
     nn::{
         optimiser::{AdamWOptimiser, AdamWParams},
         Activation, ExecutionContext, Graph, InitSettings, NetworkBuilder, Node, Shape,
@@ -21,7 +21,7 @@ const L3: usize = 32;
 const FINE_TUNING: bool = false;
 
 type Input = ChessBucketsMirroredFactorised;
-type Output = MaterialCount<8>;
+type Output = MaterialCountNoisy<8>;
 
 fn main() {
     #[rustfmt::skip]
@@ -58,19 +58,19 @@ fn main() {
             SavedFormat::new("l2b", QuantTarget::Float, Layout::Normal),
             SavedFormat::new("l3w", QuantTarget::Float, Layout::Normal),
             SavedFormat::new("l3b", QuantTarget::Float, Layout::Normal),
-            // SavedFormat::new("pst", QuantTarget::Float, Layout::Normal),
+            SavedFormat::new("pst", QuantTarget::Float, Layout::Normal),
         ],
         false,
     );
 
-    // let no_clipping = AdamWParams { min_weight: -128.0, max_weight: 128.0, ..AdamWParams::default() };
-    // let no_decay = AdamWParams { decay: 0.0, ..no_clipping };
+    let no_clipping = AdamWParams { min_weight: -128.0, max_weight: 128.0, ..AdamWParams::default() };
+    let no_decay = AdamWParams { decay: 0.0, ..no_clipping };
 
-    // trainer.optimiser_mut().set_params_for_weight("l2w", no_clipping);
-    // trainer.optimiser_mut().set_params_for_weight("l2b", no_clipping);
-    // trainer.optimiser_mut().set_params_for_weight("l3w", no_clipping);
-    // trainer.optimiser_mut().set_params_for_weight("l3b", no_clipping);
-    // trainer.optimiser_mut().set_params_for_weight("pst", no_decay);
+    trainer.optimiser_mut().set_params_for_weight("l2w", no_clipping);
+    trainer.optimiser_mut().set_params_for_weight("l2b", no_clipping);
+    trainer.optimiser_mut().set_params_for_weight("l3w", no_clipping);
+    trainer.optimiser_mut().set_params_for_weight("l3b", no_clipping);
+    trainer.optimiser_mut().set_params_for_weight("pst", no_decay);
 
     // trainer.load_from_checkpoint("checkpoints/<NAME>");
 
@@ -128,11 +128,11 @@ fn build_network(num_inputs: usize, output_buckets: usize, hl: usize) -> (Graph,
     let l1 = builder.new_affine("l1", hl, output_buckets * L2);
     let l2 = builder.new_affine("l2", L2, output_buckets * L3);
     let l3 = builder.new_affine("l3", L3, output_buckets);
-    // let pst = builder.new_weights(
-    //     "pst",
-    //     Shape::new(output_buckets, num_inputs),
-    //     InitSettings::Normal { mean: 0.1, stdev: 1.0 / 64.0 },
-    // );
+    let pst = builder.new_weights(
+        "pst",
+        Shape::new(output_buckets, num_inputs),
+        InitSettings::Normal { mean: 0.1, stdev: 1.0 / 64.0 },
+    );
 
     // inference
     let out = l0.forward_sparse_dual_with_activation(stm, nstm, Activation::CReLU);
@@ -141,8 +141,8 @@ fn build_network(num_inputs: usize, output_buckets: usize, hl: usize) -> (Graph,
     let out = l2.forward(out).select(buckets).activate(Activation::SCReLU);
     let out = l3.forward(out).select(buckets);
 
-    // let pst_out = pst.matmul(stm).select(buckets) - pst.matmul(nstm).select(buckets);
-    // let out = out + pst_out;
+    let pst_out = pst.matmul(stm).select(buckets) - pst.matmul(nstm).select(buckets);
+    let out = out + pst_out;
 
     let pred = out.activate(Activation::Sigmoid);
     pred.mse(targets);
