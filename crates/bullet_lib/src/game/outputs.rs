30f60d8cc61b298899b75b9689b1b0892eb66c31
Cosmo Bobak <cbobak@esri.com>
2025-04-15 15:48:04 +0100

    remove crap buckets

--- crates/bullet_lib/src/game/outputs.rs
+++ crates/bullet_lib/src/game/outputs.rs
@@ -27,30 +27,6 @@ impl<const N: usize> OutputBuckets<ChessBoard> for MaterialCount<N> {
     }
 }
 
-#[derive(Clone, Copy, Default)]
-pub struct MaterialCountFarseer;
-impl OutputBuckets<ChessBoard> for MaterialCountFarseer {
-    const BUCKETS: usize = 8;
-
-    fn bucket(&self, pos: &ChessBoard) -> u8 {
-        const TABLE: [u8; 33] = [
-            0,
-            0, 0, 0, 0, 0, 0, // 1, 2, 3, 4, 5, 6
-            0, 0, 0, 0, // 7, 8, 9, 10
-            1, 1, 1,
-            2, 2, 2,
-            3, 3, 3,
-            4, 4, 4,
-            5, 5, 5,
-            6, 6, 6,
-            7, 7, 7, 7
-        ];
-        // const int bucket = bucketNo[pos.count<ALL_PIECES>()];
-        let index = pos.occ().count_ones() as usize;
-        TABLE[index.min(32)]
-    }
-}
-
 impl<const N: usize> OutputBuckets<MarlinFormat> for MaterialCount<N> {
     const BUCKETS: usize = N;
 
