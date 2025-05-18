c775c5f30102ce43d41cbc5ba4fff4b275daa3bc
cosmobobak <cosmobobak@gmail.com>
2025-05-18 17:14:50 +0100

    delenda post-train

--- examples/viridithas.rs
+++ examples/viridithas.rs
@@ -19,7 +19,7 @@ const HL: usize = 2048;
 const L2: usize = 16;
 const L3: usize = 32;
 
-const FINE_TUNING: bool = false;
+const FINE_TUNING: bool = true;
 
 type Input = ChessBucketsMirroredFactorised;
 type Output = MaterialCount<8>;
@@ -51,7 +51,7 @@ fn main() {
         AdamWParams::default(),
         inputs,
         output_buckets,
-        ["l0w", "l0b", "l1w", "l1b", "l2xw", "l2fw", "l2xb", "l2fb", "l3xw", "l3fw", "l3xb", "l3fb"]
+        ["l0w", "l0b", "l1w", "l1b", "l2w", "l2b", "l3w", "l3b"]
             .map(SavedFormat::id)
             .to_vec(),
         false,
@@ -59,16 +59,13 @@ fn main() {
 
     let no_clipping = AdamWParams { min_weight: -128.0, max_weight: 128.0, ..AdamWParams::default() };
 
-    trainer.optimiser_mut().set_params_for_weight("l2xw", no_clipping);
-    trainer.optimiser_mut().set_params_for_weight("l2xb", no_clipping);
-    trainer.optimiser_mut().set_params_for_weight("l3xw", no_clipping);
-    trainer.optimiser_mut().set_params_for_weight("l3xb", no_clipping);
-    trainer.optimiser_mut().set_params_for_weight("l2fw", no_clipping);
-    trainer.optimiser_mut().set_params_for_weight("l2fb", no_clipping);
-    trainer.optimiser_mut().set_params_for_weight("l3fw", no_clipping);
-    trainer.optimiser_mut().set_params_for_weight("l3fb", no_clipping);
+    trainer.optimiser_mut().set_params_for_weight("l2w", no_clipping);
+    trainer.optimiser_mut().set_params_for_weight("l2b", no_clipping);
+    trainer.optimiser_mut().set_params_for_weight("l3w", no_clipping);
+    trainer.optimiser_mut().set_params_for_weight("l3b", no_clipping);
 
     // trainer.load_from_checkpoint("checkpoints/scorpio-200");
+    trainer.optimiser_mut().load_weights_from_file("checkpoints/delenda-800/delenda-b800-merged.raw").unwrap();
 
     let initial_lr;
     let final_lr;
@@ -84,7 +81,7 @@ fn main() {
     }
 
     let schedule = TrainingSchedule {
-        net_id: "scorpio".into(),
+        net_id: "legion".into(),
         steps: TrainingSteps {
             batch_size: 16_384,
             batches_per_superbatch: 6104,
@@ -92,10 +89,10 @@ fn main() {
             end_superbatch: sbs,
         },
         eval_scale: 400.0,
-        wdl_scheduler: wdl::LinearWDL { start: 0.4, end: 0.6 },
+        wdl_scheduler: wdl::ConstantWDL { value: 0.4 },
         lr_scheduler: lr::Warmup {
             inner: lr::LinearDecayLR { initial_lr, final_lr, final_superbatch: sbs },
-            warmup_batches: 800,
+            warmup_batches: 1600,
         },
         save_rate: 200,
     };
@@ -104,7 +101,7 @@ fn main() {
 
     // let data_loader = loader::DirectSequentialDataLoader::new(&["data/dataset.bin"]);
     let data_loader = loader::ViriBinpackLoader::new(
-        "data/dataset2.viriformat",
+        "data/dataset3.viriformat",
         512,
         4,
         viriformat::dataformat::Filter {
@@ -127,9 +124,9 @@ fn main() {
         "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
         "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
     ] {
-        let eval = trainer.eval_raw_output_for_node(fen, value_node);
+        let eval = trainer.eval(fen);
         println!("FEN: {fen}");
-        println!("EVAL: {}", 400.0 * eval[0]);
+        println!("EVAL: {}", 400.0 * eval);
     }
 }
 
@@ -145,10 +142,8 @@ fn build_network(num_inputs: usize, max_active: usize, output_buckets: usize, hl
     // trainable weights
     let l0 = builder.new_affine("l0", num_inputs, hl);
     let l1 = builder.new_affine("l1", hl, output_buckets * L2);
-    let l2x = builder.new_affine("l2x", L2, output_buckets * L3);
-    let l2f = builder.new_affine("l2f", L2, L3);
-    let l3x = builder.new_affine("l3x", L3, output_buckets);
-    let l3f = builder.new_affine("l3f", L3, 1);
+    let l2 = builder.new_affine("l2", L2, output_buckets * L3);
+    let l3 = builder.new_affine("l3", L3, output_buckets);
 
     // 32 + 32 due to feature factoriser
     l0.init_with_effective_input_size(64);
@@ -159,14 +154,8 @@ fn build_network(num_inputs: usize, max_active: usize, output_buckets: usize, hl
     let accumulator = stm_subnet.concat(ntm_subnet);
 
     let l1_out = l1.forward(accumulator).select(buckets).screlu();
-
-    let l2x_out = l2x.forward(l1_out).select(buckets);
-    let l2f_out = l2f.forward(l1_out);
-    let l2_out = (l2x_out + l2f_out).screlu();
-
-    let l3x_out = l3x.forward(l2_out).select(buckets);
-    let l3f_out = l3f.forward(l2_out);
-    let l3_out = (l3x_out + l3f_out).sigmoid();
+    let l2_out = l2.forward(l1_out).select(buckets).screlu();
+    let l3_out = l3.forward(l2_out).select(buckets).sigmoid();
 
     l3_out.squared_error(targets);
 
