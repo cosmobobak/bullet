86405446fe610151e805b4e6f6ad9db3b056dce0
cosmobobak <cosmobobak@gmail.com>
2026-01-20 02:13:53 +0000

    finetune `flounce`

--- examples/viridithas.rs
+++ examples/viridithas.rs
@@ -45,8 +45,8 @@ const BATCH_GLOM: usize = 4;
 fn main() {
     // hyperparams to fiddle with
     let dataset_path = "data/all.vf";
-    let initial_lr = 0.001;
-    let superbatches = 800;
+    let initial_lr = 0.000025;
+    let superbatches = 400;
     let lr_scheduler = lr::Warmup {
         inner: lr::CosineDecayLR {
             initial_lr,
@@ -55,8 +55,8 @@ fn main() {
         },
         warmup_batches: 1600,
     };
-    let wdl_scheduler = wdl::LinearWDL { start: 0.4, end: 1.0 };
-    // let wdl_scheduler = wdl::ConstantWDL { value: 1.0 };
+    // let wdl_scheduler = wdl::LinearWDL { start: 0.4, end: 1.0 };
+    let wdl_scheduler = wdl::ConstantWDL { value: 1.0 };
 
     let mut saves = ["l0w", "l0b", "l1w", "l1b", "l2xw", "l2fw", "l2xb", "l2fb", "l3xw", "l3fw", "l3xb", "l3fb"]
         .map(SavedFormat::id)
@@ -175,7 +175,7 @@ fn main() {
     trainer.optimiser.set_params_for_weight("l3fb", no_clipping);
 
     let schedule = TrainingSchedule {
-        net_id: "flounce".to_string(),
+        net_id: "flounce-finetune".to_string(),
         eval_scale: 400.0,
         steps: TrainingSteps {
             batch_size: 16_384 * BATCH_GLOM,
@@ -217,7 +217,7 @@ fn main() {
         },
     );
 
-    // trainer.load_from_checkpoint("checkpoints/ornament-800");
+    trainer.load_from_checkpoint("checkpoints/flounce-800");
 
     trainer.run(&schedule, &settings, &dataloader);
 }
