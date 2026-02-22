0c7d1cf5435c89c8435d933875fb3b8e115344e1
cosmobobak <cosmobobak@gmail.com>
2026-02-22 20:01:38 +0000

    code cleanup

--- examples/viridithas.rs
+++ examples/viridithas.rs
@@ -17,162 +17,6 @@ use bullet_lib::{
     value::ValueTrainerBuilder,
 };
 
-use acyclib::{
-    dag::NodeId,
-    device::function::{self, DeviceFunction},
-    graph::{
-        Graph, GraphNodeIdTy,
-        ir::{
-            BackendMarker, GraphIR, GraphIRError,
-            node::AnnotatedNode,
-            operation::{GraphIROperationBase, GraphIROperationCompilable, GraphIROperationError, util},
-        },
-    },
-};
-use bullet_cuda_backend::{
-    CudaDevice,
-    kernel::{Expr, Kernel, KernelArgs, KernelInput},
-};
-
-#[derive(Debug)]
-struct BCELogitsLoss {
-    logits: AnnotatedNode,
-    target: AnnotatedNode,
-}
-
-impl<B: BackendMarker> GraphIROperationBase<B> for BCELogitsLoss {
-    fn nodes(&self) -> Vec<AnnotatedNode> {
-        vec![self.logits, self.target]
-    }
-
-    fn output_shape(&self, ir: &GraphIR<B>) -> Result<Shape, GraphIRError> {
-        util::check_same_batching(ir, &[&self.logits, &self.target])?;
-        util::check_dense_eq(ir, &self.logits, true)?;
-        util::check_dense_eq(ir, &self.target, true)?;
-
-        if self.logits.shape == self.target.shape {
-            Ok(self.logits.shape)
-        } else {
-            Err(GraphIRError::Op(GraphIROperationError::MismatchedInputShapes(vec![
-                self.logits.shape,
-                self.target.shape,
-            ])))
-        }
-    }
-}
-
-impl GraphIROperationCompilable<CudaMarker> for BCELogitsLoss {
-    fn forward_pass(&self, graph: &Graph<CudaDevice>, output_node: NodeId) -> DeviceFunction<CudaDevice> {
-        let logits = graph.get_ref(self.logits.idx, GraphNodeIdTy::Values);
-        let target = graph.get_ref(self.target.idx, GraphNodeIdTy::Values);
-        let output = graph.get_ref(output_node, GraphNodeIdTy::Values);
-
-        let mut func = DeviceFunction::default();
-
-        func.push(function::MaybeUpdateBatchSize { input: logits.clone(), output: output.clone() });
-
-        let code = "
-            __device__ float ln_sigmoid(float x) {{
-                return x >= 0.0 ? -logf(1.0 + expf(-x)) : x - logf(1.0 + expf(x));
-            }}
-
-            extern \"C\" __global__ void kernel(const int size, const float* logits, const float* target, float* output)
-            {{
-                const int tid = blockIdx.x * blockDim.x + threadIdx.x;
-
-                if (tid < size)
-                {{
-                    const float inp = logits[tid];
-                    const float tar = target[tid];
-
-                    output[tid] = tar == 0.0f
-                        ? -ln_sigmoid(-inp)
-                        : tar == 1.0f
-                            ? -ln_sigmoid(inp)
-                            : tar * (logf(tar) - ln_sigmoid(inp)) + (1.0 - tar) * (logf(1.0 - tar) - ln_sigmoid(-inp));
-                }}
-            }}";
-
-        let threads = Expr::Const(512);
-        let size = Expr::Var * logits.single_size() as i32;
-        let blocks = (size.clone() + threads.clone() - 1) / threads.clone();
-        let grid_dim = [blocks, Expr::Const(1), Expr::Const(1)];
-        let block_dim = [threads, Expr::Const(1), Expr::Const(1)];
-
-        let layout = None;
-        let batched = logits.batch_size().is_some();
-        let shape = logits.shape();
-        let inputs = vec![
-            KernelInput::Size(size),
-            KernelInput::Slice { slice: logits, layout, mutable: false, batched, shape },
-            KernelInput::Slice { slice: target, layout, mutable: false, batched, shape },
-            KernelInput::Slice { slice: output, layout, mutable: true, batched, shape },
-        ];
-
-        let args = KernelArgs { grid_dim, block_dim, shared_mem_bytes: Expr::Const(0), inputs };
-
-        let kernel = unsafe { Kernel::new("BCELogitsLossFwd".to_string(), code.into(), args) };
-
-        func.push(kernel.unwrap());
-
-        func
-    }
-
-    fn backward_pass(&self, graph: &Graph<CudaDevice>, output_node: NodeId) -> DeviceFunction<CudaDevice> {
-        let logits = graph.get_ref(self.logits.idx, GraphNodeIdTy::Values);
-        let target = graph.get_ref(self.target.idx, GraphNodeIdTy::Values);
-        let output_grad = graph.get_ref(output_node, GraphNodeIdTy::Gradients);
-
-        let mut func = DeviceFunction::default();
-
-        if let Some(input_grad) = graph.maybe_get_ref(self.logits.idx, GraphNodeIdTy::Gradients) {
-            func.push(function::MaybeUpdateBatchSize { input: output_grad.clone(), output: input_grad.clone() });
-
-            let code = "
-                extern \"C\" __global__ void kernel(const int size, const float* logits, const float* target, const float* ogrd, float* igrd)
-                {{
-                    const int tid = blockIdx.x * blockDim.x + threadIdx.x;
-
-                    if (tid < size)
-                    {{
-                        const float sig = 1.0f / (1.0f + expf(-logits[tid]));
-                        const float tar = target[tid];
-                        igrd[tid] += ogrd[tid] * (-tar * (1.0 - sig) + (1.0 - tar) * sig);
-                    }}
-                }}";
-
-            let threads = Expr::Const(512);
-            let size = Expr::Var * logits.single_size() as i32;
-            let blocks = (size.clone() + threads.clone() - 1) / threads.clone();
-            let grid_dim = [blocks, Expr::Const(1), Expr::Const(1)];
-            let block_dim = [threads, Expr::Const(1), Expr::Const(1)];
-
-            let layout = None;
-            let batched = logits.batch_size().is_some();
-            let shape = logits.shape();
-            let inputs = vec![
-                KernelInput::Size(size),
-                KernelInput::Slice { slice: logits, layout, mutable: false, batched, shape },
-                KernelInput::Slice { slice: target, layout, mutable: false, batched, shape },
-                KernelInput::Slice { slice: output_grad, layout, mutable: false, batched, shape },
-                KernelInput::Slice { slice: input_grad, layout, mutable: true, batched, shape },
-            ];
-
-            let args = KernelArgs { grid_dim, block_dim, shared_mem_bytes: Expr::Const(0), inputs };
-
-            let kernel = unsafe { Kernel::new("BCELogitsLossBwd".to_string(), code.into(), args) };
-
-            func.push(kernel.unwrap());
-        }
-
-        if graph.maybe_get_ref(self.target.idx, GraphNodeIdTy::Gradients).is_some() {
-            panic!("Unsupported!");
-        }
-
-        func
-    }
-}
-
 const L1: usize = 2560;
 const L2: usize = 16;
 const L3: usize = 32;
@@ -212,7 +56,6 @@ fn main() {
         warmup_batches: 1600,
     };
     let wdl_scheduler = wdl::LinearWDL { start: 0.4, end: 1.0 };
-    // let wdl_scheduler = wdl::ConstantWDL { value: 1.0 };
 
     let mut saves = ["l0w", "l0b", "l1w", "l1b", "l2xw", "l2fw", "l2xb", "l2fb", "l3xw", "l3fw", "l3xb", "l3fb"]
         .map(SavedFormat::id)
@@ -231,14 +74,12 @@ fn main() {
     });
 
     let mut trainer = ValueTrainerBuilder::default()
-        // .full_output()
         .inputs(ChessBucketsMirrored::new(BUCKET_LAYOUT))
         .dual_perspective()
         .optimiser(AdamW)
         .output_buckets(MaterialCount::<NUM_OUTPUT_BUCKETS>)
         .save_format(&saves)
         .build_custom(|builder, (stm_inputs, ntm_inputs, output_buckets), targets| {
-            // builder.dump_graphviz("viz.txt");
             // input layer factoriser
             let l0f = builder.new_weights("l0f", Shape::new(L1, 768), InitSettings::Zeroed);
             let expanded_factoriser = l0f.repeat(NUM_INPUT_BUCKETS);
@@ -318,8 +159,6 @@ fn main() {
 
                 (l3_out, loss)
             } else {
-                // let loss =
-                //     builder.apply(BCELogitsLoss { logits: l3_out.annotated_node(), target: targets.annotated_node() });
                 let loss = l3_out.sigmoid().squared_error(targets);
 
                 let loss = loss + 0.005 * l0_out_norm;
@@ -332,16 +171,9 @@ fn main() {
     trainer.optimiser.set_params_for_weight("l1w", adamw);
     trainer.optimiser.set_params_for_weight("l1b", adamw);
     let no_clipping = AdamWParams { min_weight: -128.0, max_weight: 128.0, ..adamw };
-    trainer.optimiser.set_params_for_weight("l0w", no_clipping);
-    trainer.optimiser.set_params_for_weight("l0f", no_clipping);
-    trainer.optimiser.set_params_for_weight("l2xw", no_clipping);
-    trainer.optimiser.set_params_for_weight("l2xb", no_clipping);
-    trainer.optimiser.set_params_for_weight("l2fw", no_clipping);
-    trainer.optimiser.set_params_for_weight("l2fb", no_clipping);
-    trainer.optimiser.set_params_for_weight("l3xw", no_clipping);
-    trainer.optimiser.set_params_for_weight("l3xb", no_clipping);
-    trainer.optimiser.set_params_for_weight("l3fw", no_clipping);
-    trainer.optimiser.set_params_for_weight("l3fb", no_clipping);
+    for name in ["l0w", "l0f", "l2xw", "l2xb", "l2fw", "l2fb", "l3xw", "l3xb", "l3fw", "l3fb"] {
+        trainer.optimiser.set_params_for_weight(name, no_clipping);
+    }
 
     let schedule = TrainingSchedule {
         net_id: "click".to_string(),
@@ -359,34 +191,17 @@ fn main() {
 
     let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 32 };
 
-    // let dataloader = bullet_lib::value::loader::DirectSequentialDataLoader::new(&[dataset_path]);
     let dataloader = bullet_lib::value::loader::ViriBinpackLoader::new(
         dataset_path,
         4096,
         16,
         viriformat::dataformat::Filter {
-            min_ply: 16,
-            min_pieces: 4,
             max_eval: 20_000,
-            filter_tactical: true,
-            filter_check: true,
-            filter_castling: false,
-            max_eval_incorrectness: u32::MAX,
-
-            // from Default::default()
             random_fen_skipping: true,
             random_fen_skip_probability: 9.0 / 10.0,
-            wdl_filtered: false,
-            wdl_model_params_a: [6.871_558_62, -39.652_263_91, 90.684_603_52, 170.669_963_64],
-            wdl_model_params_b: [-7.198_907_10, 56.139_471_85, -139.910_911_83, 182.810_074_27],
-            material_min: 17,
-            material_max: 78,
-            mom_target: 58,
-            wdl_heuristic_scale: 1.5,
+            ..Default::default()
         },
     );
-
-    // trainer.load_from_checkpoint("checkpoints/flounce-800");
 
     trainer.run(&schedule, &settings, &dataloader);
 }
@@ -406,6 +221,5 @@ fn exp(x: GraphBuilderNode<'_, CudaMarker>) -> GraphBuilderNode<'_, CudaMarker> 
 }
 
 fn hard_swish(x: GraphBuilderNode<'_, CudaMarker>) -> GraphBuilderNode<'_, CudaMarker> {
-    let relu6 = ((x + 3.0) / 6.0).crelu() * 6.0;
-    x * relu6 / 6.0
+    x * ((x + 3.0) / 6.0).crelu()
 }
