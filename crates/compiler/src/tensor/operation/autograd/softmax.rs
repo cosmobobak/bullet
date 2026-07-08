cdf218f749f244c46688bdd6f9c244a49b7ceb47
cosmo <cosmo@asteri.sm>
2026-07-08 22:39:39 +0100

    compute log-softmax directly in softmax CE loss

--- crates/compiler/src/tensor/operation/autograd/softmax.rs
+++ crates/compiler/src/tensor/operation/autograd/softmax.rs
@@ -26,7 +26,23 @@ impl CustomAutograd for SoftmaxCrossEntropyLoss {
 
     fn forward<'a>(&self, inputs: Vec<TNode<'a>>) -> Result<Vec<TNode<'a>>, IRTrace> {
         let [input, target] = inputs[..] else { return Err("Invalid number of inputs!".into()) };
-        (-(target * input.softmax(self.axis_size)?.log()?)?).map(|x| vec![x])
+
+        // log-softmax computed directly, since log(softmax(x)) produces -inf
+        // (and then 0 * -inf = NaN) when a softmax entry underflows to zero
+        let batch_size = self.batch_size;
+        let inner = Size::from(self.axis_size);
+        let max = input
+            .reduce_max([batch_size, inner], 1)?
+            .broadcast([batch_size, 1.into()], 1, self.axis_size)?;
+        let shifted = (input - max)?;
+        let log_denom = shifted
+            .exp()?
+            .reduce_sum([batch_size, inner], 1)?
+            .log()?
+            .broadcast([batch_size, 1.into()], 1, self.axis_size)?;
+        let log_softmax = (shifted - log_denom)?;
+
+        (-(target * log_softmax)?).map(|x| vec![x])
     }
 
     fn backward<'a>(&self, inputs: Vec<TNode<'a>>, output_grads: Vec<TNode<'a>>) -> Result<Vec<TNode<'a>>, IRTrace> {
